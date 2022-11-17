de5c4a17f8a5dcabdd2540b9b157edf5703aab7b
Tim Wanner <info@tim-wanner.de>
2022-11-17 20:49:16 +0100

    Try2 subdomain handling

--- page.js
+++ page.js
@@ -4,33 +4,39 @@ document.addEventListener("DOMContentLoaded", () => {
     const mainText = document.getElementById("maintext")
     const mailButton = document.getElementById("mail-button")
 
-    const emailTo = "info@" + host
-    const emailSub = "Request regarding"
-
     const processHost = () => {
         const host = document.domain
         const hostArray = host.split(".")
 
         if(hostArray.length >= 3){
             hostArray.shift()
-            hostArray.join(".")
+            const hostAsString = hostArray.join(".")
+            console.log("loop")
+            return hostAsString 
 
             
+
         } else {
             hostArray.join(".")
         }
 
-        return newHost
+           
     }
 
-    const host = processHost().newHost
+    // const host = processHost().hostAsString
+    // const emailTo = "info@" + host
+    const emailSub = "Request regarding"
+    //console.log(newHost)
+
+    processHost()
+    console.log("Processhost called")
     
     
     const sendMail = () => {
         window.location.href = "mailto:"+emailTo+'?subject='+emailSub
     }
 
-    mainText.innerText = host + " is currently offline!"
+    // mainText.innerText = host + " is currently offline!"
 
     mailButton.addEventListener("click", (event) => {
         event.preventDefault()
