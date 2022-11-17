e86130d89bc4cf65aec01b120f4750c44f477b7b
Tim Wanner <info@tim-wanner.de>
2022-11-17 20:37:57 +0100

    subDomain handling implemented - testing

--- page.js
+++ page.js
@@ -4,16 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const mainText = document.getElementById("maintext")
     const mailButton = document.getElementById("mail-button")
 
-    const emailTo = "info@" + document.location.host
+    const emailTo = "info@" + host
     const emailSub = "Request regarding"
 
+    const processHost = () => {
+        const host = document.domain
+        const hostArray = host.split(".")
+
+        if(hostArray.length >= 3){
+            hostArray.shift()
+            hostArray.join(".")
+
+            
+        } else {
+            hostArray.join(".")
+        }
+
+        return newHost
+    }
+
+    const host = processHost().newHost
     
     
     const sendMail = () => {
         window.location.href = "mailto:"+emailTo+'?subject='+emailSub
     }
 
-    mainText.innerText = document.location.host + " is currently offline!"
+    mainText.innerText = host + " is currently offline!"
 
     mailButton.addEventListener("click", (event) => {
         event.preventDefault()
